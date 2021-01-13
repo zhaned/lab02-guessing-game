44df2552129cf49a91578c7fc2c7be6502799422
zhaned <zhanedmond@gmail.com>
2021-01-13 00:42:48 -0800

    added functions to for checking guess remaining

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 // import functions and grab DOM elements
-import { compareNumbers } from './utils.js';
+import { compareNumbers, inputResponse, guessCount } from './utils.js';
 
 const guessButton = document.getElementById('guess-button');
 const userInput = document.getElementById('user-input');
+const guessHint = document.getElementById('guess-hint');
+const guessDisplay = document.getElementById('guess-checker');
 // initialize state
 let guessesLeft = 4;
 let realNumber = Math.ceil(Math.random() * 20);
 // set event listeners to update state and DOM
 guessButton.addEventListener('click', () =>{
     const inputNumber = Number(userInput.value);
-    compareNumbers(inputNumber, realNumber);
-    console.log(inputNumber, realNumber, guessesLeft);
+    --guessesLeft;
+    guessDisplay.textContent = guessCount(guessesLeft);
+    guessHint.textContent = inputResponse(compareNumbers(inputNumber, realNumber));
+    if (guessesLeft === 0 || compareNumbers(inputNumber, realNumber) === 0){
+        guessButton.style.display = 'none';
+        if (compareNumbers(inputNumber, realNumber) === 0){
+            guessHint.textContent = 'You guessed correct!!';
+        }
+        else {
+            guessHint.textContent = 'Out of guesses! Thanks for playing!';
+        }
+    }
+    console.log(realNumber);
 });
--- utils.js
+++ utils.js
@@ -9,3 +9,21 @@ export function compareNumbers(guess, correctNumber){
         return 0;
     }
 }
+
+export function inputResponse(results){
+    if (results === 0){
+        return 'You did it! You guessed right!';
+    }
+    else if (results > 0){
+        return 'Your guess was too high!';
+    }
+    else {
+        return 'Your guess was too low!';
+    }
+}
+
+export function guessCount(guessesLeft){
+    let string = 'Guesses left: ';
+    string += guessesLeft;
+    return string;
+}
